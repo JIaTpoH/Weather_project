4dfb870466d22b3c18afddb679572a97873c0494
Dmitri Mandaji <badmopss@gmail.com>
2023-05-18 17:06:42 +0200

    add API key

--- wetter.js
+++ wetter.js
@@ -1,1 +1,15 @@
+const apiKey = "fbe2c2fdac7fe2e53740d0f700d43424";
+function getWeatherData() {
+   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
 
+   fetch(apiUrl)
+   .then(response => response.json())
+   .then(data => {
+    console.log(data);
+   })
+   .catch(error => {
+    console.log("Error", error);
+   })
+}
+
+getWeatherData();
