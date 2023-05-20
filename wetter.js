8e272256d5747355dc44609ac75ecff0518b483e
Dmitri Mandaji <badmopss@gmail.com>
2023-05-20 13:02:38 +0200

    add geolocation and apiKey

--- wetter.js
+++ wetter.js
@@ -1,12 +1,30 @@
 
-var todayLink = document.getElementById("today");
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
 
 
-todayLink.addEventListener("click", function(event) {
-  event.preventDefault();
-  document.getElementById("city").textContent = "Ваш город: Франкфурт";
-  document.getElementById("temperature").textContent = "Текущая температура: 25°C";
-  document.getElementById("wind").textContent = "Скорость ветра: 10 м/c";
-  document.getElementById("humidity").textContent = "Влажность: 70%";
+    const apiKey = 'fbe2c2fdac7fe2e53740d0f700d43424';
+    let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
 
-});
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', weatherUrl, true);
+    
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+      
+        const temperature = response.main.temp;
+        const windSpeed = response.wind.speed;
+        const humidity = response.main.humidity;
+
+        document.getElementById('temperature').textContent = 'Текущая температура: ' + temperature + '°C';
+        document.getElementById('wind').textContent = 'Скорость ветра: ' + windSpeed + ' м/c';
+        document.getElementById('humidity').textContent = 'Влажность: ' + humidity + '%';
+      }
+    };
+
+    xhr.send();
+  });
+}
